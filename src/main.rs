4d98ac02e75904a59e075f514d3587b9995930e0
Matt Kline <matt@bitbashing.io>
2023-05-25 09:36:26 -0700

    With color!

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::{
-    fmt, fs,
+    fs,
     io::{prelude::*, Cursor, SeekFrom},
 };
 
@@ -35,11 +35,6 @@ enum Color {
     Never,
 }
 
-fn doomstr(d: &[u8]) -> &str {
-    let nulterm = d.iter().take_while(|b| **b != 0).count();
-    std::str::from_utf8(&d[..nulterm]).expect("Non-ASCII in WAD")
-}
-
 fn map_wad(wad: &Utf8Path) -> Result<Mmap> {
     let fd = fs::File::open(wad).context("Couldn't open WAD")?;
     let mmap = unsafe { Mmap::map(&fd).context("Couldn't map WAD")? };
@@ -51,6 +46,11 @@ struct Wadinfo {
     magic: [u8; 4],
     numlumps: u32,
     infotableofs: u32,
+}
+
+fn doomstr(d: &[u8]) -> &str {
+    let nulterm = d.iter().take_while(|b| **b != 0).count();
+    std::str::from_utf8(&d[..nulterm]).expect("Non-ASCII in WAD")
 }
 
 fn go(args: Args) -> Result<()> {
@@ -67,30 +67,45 @@ fn go(args: Args) -> Result<()> {
     debug!("{} lumps", info.numlumps);
     if max_level() == LevelFilter::Trace {
         for lump in &dir {
-            trace!("  {}", doomstr(&lump.name));
+            trace!("  {}", lump.name());
         }
     }
 
-    // TODO Palette loading
+    let palette = dir
+        .iter()
+        .find(|l| l.name() == "PLAYPAL")
+        .expect("No palette");
+    let palette = read_palette(curse, palette)?;
+
+    let used_colors: &mut [bool] = &mut [false; 256];
 
     let sprites = dir
         .iter()
-        .skip_while(|l| doomstr(&l.name) != "S_START")
+        .skip_while(|l| l.name() != "S_START")
         .skip(1)
-        .take_while(|l| doomstr(&l.name) != "S_END");
+        .take_while(|l| l.name() != "S_END");
 
     info!("Sprites:");
     for s in sprites {
-        info!("  {}", doomstr(&s.name));
-        save_sprite(curse, s)?;
+        info!("  {}", s.name());
+        save_sprite(curse, s, &palette, used_colors)?;
     }
 
-    let faces = dir.iter().filter(|l| doomstr(&l.name).starts_with("STF"));
+    let faces = dir.iter().filter(|l| l.name().starts_with("STF"));
     info!("Faces:");
     for f in faces {
-        info!("  {}", doomstr(&f.name));
-        save_sprite(curse, f)?;
+        info!("  {}", f.name());
+        save_sprite(curse, f, &palette, used_colors)?;
     }
+
+    // We can use these for transparency
+    let unused_color_indexes = used_colors
+        .iter()
+        .enumerate()
+        .filter(|(_, c)| !**c)
+        .map(|(i, _)| i)
+        .collect::<Vec<_>>();
+    debug!("Unused colors: {:?}", unused_color_indexes);
 
     Ok(())
 }
@@ -98,17 +113,13 @@ fn go(args: Args) -> Result<()> {
 #[binread]
 struct Filelump {
     filepos: u32,
-    size: u32,
-    name: [u8; 8],
+    _size: u32,
+    namebuf: [u8; 8],
 }
 
-impl fmt::Debug for Filelump {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        f.debug_struct("Filelump")
-            .field("filepos", &self.filepos)
-            .field("size", &self.size)
-            .field("name", &doomstr(&self.name))
-            .finish()
+impl Filelump {
+    fn name(&self) -> &str {
+        doomstr(&self.namebuf)
     }
 }
 
@@ -116,9 +127,26 @@ fn read_directory(c: &mut Cursor<Mmap>, wi: &Wadinfo) -> Result<Vec<Filelump>> {
     c.seek(SeekFrom::Start(wi.infotableofs as u64))?;
     let mut lumps = Vec::with_capacity(wi.numlumps as usize);
     for _i in 0..wi.numlumps {
-        lumps.push(c.read_le()?);
+        let lump = c.read_le()?;
+        lumps.push(lump);
     }
     Ok(lumps)
+}
+
+#[derive(BinRead, Debug)]
+struct PaletteColor {
+    r: u8,
+    g: u8,
+    b: u8,
+}
+
+fn read_palette(c: &mut Cursor<Mmap>, lump: &Filelump) -> Result<Vec<PaletteColor>> {
+    c.seek(SeekFrom::Start(lump.filepos as u64))?;
+    let mut pal = Vec::with_capacity(256);
+    for _ in 0..256 {
+        pal.push(c.read_le()?);
+    }
+    Ok(pal)
 }
 
 #[derive(BinRead, Debug)]
@@ -132,13 +160,18 @@ struct PatchHeader {
 }
 
 #[binrw]
-struct Post {
+struct PostHeader {
     topdelta: u8,
     #[br(pad_after = 1)]
     length: u8,
 }
 
-fn save_sprite(c: &mut Cursor<Mmap>, sprite: &Filelump) -> Result<()> {
+fn save_sprite(
+    c: &mut Cursor<Mmap>,
+    sprite: &Filelump,
+    palette: &[PaletteColor],
+    used_colors: &mut [bool],
+) -> Result<()> {
     let base = sprite.filepos as u64;
     c.seek(SeekFrom::Start(base))?;
 
@@ -146,14 +179,14 @@ fn save_sprite(c: &mut Cursor<Mmap>, sprite: &Filelump) -> Result<()> {
     trace!("    {header:?}");
 
     // TODO: Glorious palette-indexed PNG
-    let mut img = image::GrayImage::new(header.width as u32, header.height as u32);
+    let mut img = image::RgbaImage::new(header.width as u32, header.height as u32);
 
     for (x, col) in header.columnofs.iter().enumerate() {
         // trace!("      column {x}:");
         c.seek(SeekFrom::Start(base + *col as u64))?;
         loop {
             // trace!("At {}", c.position());
-            let post: Post = c.read_le()?;
+            let post: PostHeader = c.read_le()?;
             if post.topdelta == 255 {
                 // trace!("        EOC");
                 break;
@@ -166,15 +199,18 @@ fn save_sprite(c: &mut Cursor<Mmap>, sprite: &Filelump) -> Result<()> {
             );
             */
             for dy in 0..post.length {
-                let px = read_u8(c)?;
+                let px = read_u8(c)? as usize;
+                used_colors[px] = true;
                 // trace!("          [{}] = {px}", post.topdelta + dy);
-                img.put_pixel(x as u32, (post.topdelta + dy) as u32, [px].into());
+                let pal = &palette[px];
+                let pix = [pal.r, pal.g, pal.b, 255];
+                img.put_pixel(x as u32, (post.topdelta + dy) as u32, pix.into());
             }
             let _pad = read_u8(c)?;
         }
     }
 
-    let outname = doomstr(&sprite.name).to_owned() + ".png";
+    let outname = sprite.name().to_owned() + ".png";
     img.save(outname)?;
 
     Ok(())
@@ -234,7 +270,7 @@ fn init_logger(args: &Args) {
         Color::Never => ColorChoice::Never,
     };
 
-    TermLogger::init(level, config.clone(), TerminalMode::Stderr, color)
+    TermLogger::init(level, config, TerminalMode::Stderr, color)
         .context("Couldn't init logger")
         .unwrap()
 }
